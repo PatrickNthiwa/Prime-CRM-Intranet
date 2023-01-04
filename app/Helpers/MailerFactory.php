db629858d85ff48bc331155cbb8990a6eb06cc86
patrick-Wambua <pwnthiwa@gmail.com>
2023-01-04 15:26:57 +0300

    Email updates

--- app/Helpers/MailerFactory.php
+++ app/Helpers/MailerFactory.php
@@ -202,4 +202,36 @@ class MailerFactory
             die("Mailer Factory error: " . $ex->getMessage());
         }
     }
+    /**
+     * send mailbox email
+     *
+     *
+     * @param $mailbox
+     * @param $receivers
+     */
+    public function sendMailboxEmail($mailbox)
+    {
+        try {
+
+            foreach ($mailbox->receivers as $receiver) {
+
+                $user = User::find($receiver->receiver_id);
+
+                $this->mailer->send("emails.mailbox_send", ['user' => $user, 'mailbox' => $mailbox], function ($message) use ($user, $mailbox) {
+
+                    $message->from($this->fromAddress, $this->fromName)
+                        ->to($user->email)->subject($mailbox->subject);
+
+                    if($mailbox->attachments->count() > 0) {
+                        foreach($mailbox->attachments as $attachment) {
+                            $message->attach(public_path('uploads/mailbox/' . $attachment->attachment));
+                        }
+                    }
+
+                });
+            }
+        } catch (\Exception $ex) {
+            die("Mailer Factory error: " . $ex->getMessage());
+        }
+    }
 }
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -27,7 +27,7 @@
             @if(user_can('list_contacts'))
                 <li class="treeview {{ Request::segment(2) == 'contacts'? 'active':'' }}">
                     <a href="#">
-                        <i class="fa fa-address-card"></i> <span>Contacts</span>
+                        <i class="fa fa-address-card"></i> <span>Accounts</span>
                         <span class="pull-right-container">
                             <i class="fa fa-angle-left pull-right"></i>
                         </span>
@@ -68,10 +68,50 @@
                 </li>
             @endif
 
+            @if(user_can('list_emails') || user_can('compose_email'))
+                <li class="treeview {{ Request::segment(2) == 'mailbox' || strpos(Request::segment(2), "mailbox")!==FALSE? 'active':'' }}">
+                    <a href="#">
+                        <i class="fa fa-envelope"></i> <span>Mailbox</span>
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        @if(user_can('list_emails'))
+                            <li class="{{ Request::segment(2) == "mailbox" || Request::segment(3)=="" || Request::segment(3)=="Inbox"?"active":"" }}">
+                                <a href="{{ url('/admin/mailbox') }}">
+                                    Inbox
+                                    @if(count(getUnreadMessages()) > 0)
+                                        <span class="pull-right-container">
+                                            <span class="label label-primary pull-right">{{count(getUnreadMessages())}}</span>
+                                        </span>
+                                    @endif
+                                </a>
+                            </li>
+                        @endif
+                        @if(user_can('compose_email'))
+                            <li class="{{ Request::segment(2) == "mailbox-create"?"active":"" }}">
+                                <a href="{{ url('/admin/mailbox-create') }}">
+                                    Compose
+                                </a>
+                            </li>
+                        @endif
+                    </ul>
+                </li>
+            @endif
+
+            @if(user_can('show_calendar'))
+                <li class="{{ Request::segment(2) == "calendar"?"active":"" }}">
+                    <a href="{{ url('/admin/calendar') }}">
+                        <i class="fa fa-calendar"></i> <span>Calendar</span>
+                    </a>
+                </li>
+            @endif
+
             @if(\Auth::user()->is_admin == 1)
                 <li class="{{ in_array(Request::segment(2), ['users', 'permissions', 'roles'])?"active":"" }} treeview">
                     <a href="#">
-                        <i class="fa fa-users"></i> <span>Settings</span>
+                        <i class="fa fa-users"></i> <span>User Managment</span>
                         <span class="pull-right-container">
                             <i class="fa fa-angle-left pull-right"></i>
                         </span>
